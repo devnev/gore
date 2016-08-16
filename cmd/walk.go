6e718203480eeda1f9f8bda896486a0f5fc4445d
Mark Nevill <mark.nevill@gmail.com>
2016-08-17 00:09:20 +0100

    Handle errors.

--- cmd/walk.go
+++ cmd/walk.go
@@ -5,9 +5,15 @@ package cmd
 
 import (
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
+	"reflect"
+
+	"github.com/spf13/pflag"
 )
+
+var fVerbose = pflag.BoolP("verbose", "v", false, "print additional information")
 
 func walkReplace(replacer func(in []byte) []byte, paths ...string) error {
 	for _, path := range paths {
@@ -17,10 +23,22 @@ func walkReplace(replacer func(in []byte) []byte, paths ...string) error {
 			}
 			data, err := ioutil.ReadFile(path)
 			if err != nil {
-				return err
+				if *fVerbose {
+					log.Printf("Error reading %v: %v", path, err.Error())
+				}
+				return nil
 			}
-			data = replacer(data)
-			return ioutil.WriteFile(path, data, info.Mode())
+			replaced := replacer(data)
+			if reflect.DeepEqual(data, replaced) {
+				return nil
+			}
+			err = ioutil.WriteFile(path, replaced, info.Mode())
+			if err != nil {
+				if *fVerbose {
+					log.Printf("Error writing %v: %v", path, err.Error())
+				}
+			}
+			return nil
 		})
 		if err != nil {
 			return err
@@ -37,7 +55,10 @@ func walkRead(reader func(path string, in []byte), paths ...string) error {
 			}
 			data, err := ioutil.ReadFile(path)
 			if err != nil {
-				return err
+				if *fVerbose {
+					log.Printf("Error reading %v: %v", path, err.Error())
+				}
+				return nil
 			}
 			reader(path, data)
 			return nil
